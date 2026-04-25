40cfde4fd94c2f0c788bbfa165303c121fe267a2
Vedansh Saini <vedanshsaini97@gmail.com>
2026-04-25 22:45:53 +0530

    Set up routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Signpage from "./Pages/Signpage"
+
 function App() {
+
   return (
-    <div>
-      <h1>Base React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Signpage />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
